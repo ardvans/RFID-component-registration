a6923cd3951159a7117c491b499b82f99566b605
ardvans <88268989+ardvans@users.noreply.github.com>
2021-11-02 15:01:35 +0100

    hi test

--- alliander-rfid-component-registration/src/App.java
+++ alliander-rfid-component-registration/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // hallo giel
         System.out.println("Hello, World!");
-        // hi
+        // hi ik ben Ard
+
     }
 }
